d5a8c479f50e1c17450eb42301d418c18b16a3f7
github <github>
2022-10-05 22:22:12 +0900

    10_5の学習分

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
-
+import classes from "./CssModules.module.scss";
+import styled from "styled-components";
 export const App = () => {
 
     //statet定義
@@ -12,18 +13,51 @@ export const App = () => {
         setNum((prev) => prev + 1 );
     };
 
-    useEffect(() => {
-        alert();
-    }, [num]);
-
     return (
     <>
-        <h1 style={{color :"red"}}>こんにちは！</h1>
+        {/* <h1 style={{color :"red"}}>こんにちは！</h1>
         <ColoredMessage color="blue">受け渡し成功</ColoredMessage>
         <ColoredMessage color="pink">gitにあげたよ</ColoredMessage>
         <button onClick={onClickButton}>ボタン</button>
-        <p>{ num }</p>
+        <p>{ num }</p> */}
+
+        <SContainer>
+            <div>
+                <STitle>styled componentsです</STitle>
+                <SButton>ボタン</SButton>
+            </div>
+        </SContainer>
+
+        
+        
     </>
     );
 
 };
+
+const SContainer = styled.div`
+            border: solid 1px #aaa;
+            border-radius: 20px;
+            padding : 8px;
+            margin : 8px;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            `;
+
+        const STitle = styled.p`
+        margin: 0;
+        color: #aaa;
+        `;
+
+        const SButton = styled.button`
+        background-color: #bbb;
+        border: none;
+        padding: 8px;
+        border-radius: 8px;
+            &:hover {
+                background-color: #aaa;
+                color: #fff;
+                cursor: pointer;
+            }
+        `;
